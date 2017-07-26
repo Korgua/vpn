de17457bf428a7d47000e82cf5cdda9d2078417c
Korga Balázs <korga.balazs@gmail.com>
2017-07-26 23:12:28 +0200

    vpn playground

--- VPN Connection/VPN Connection/Form1.Designer.cs
+++ VPN Connection/VPN Connection/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace VPN_Connection {
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace VPN_Connection {
     partial class Form1 {
         /// <summary>
         /// Required designer variable.
@@ -9,6 +12,12 @@
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+        {
+            GraphicsPath wantedshape = new GraphicsPath();
+            wantedshape.AddEllipse(25, 25, 25,25);
+            this.Region = new Region(wantedshape);
+        }
         protected override void Dispose(bool disposing) {
             if(disposing && (components != null)) {
                 components.Dispose();
@@ -23,12 +32,51 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.vpnConnect = new System.Windows.Forms.Button();
+            this.vpmDisconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // vpnConnect
+            // 
+            this.vpnConnect.Location = new System.Drawing.Point(2, 0);
+            this.vpnConnect.Name = "vpnConnect";
+            this.vpnConnect.Size = new System.Drawing.Size(75, 23);
+            this.vpnConnect.TabIndex = 0;
+            this.vpnConnect.Text = "vpnConnect";
+            this.vpnConnect.UseVisualStyleBackColor = true;
+            this.vpnConnect.Click += new System.EventHandler(this.vpnConnect_Click);
+            // 
+            // vpmDisconnect
+            // 
+            this.vpmDisconnect.Location = new System.Drawing.Point(2, 22);
+            this.vpmDisconnect.Name = "vpmDisconnect";
+            this.vpmDisconnect.Size = new System.Drawing.Size(98, 23);
+            this.vpmDisconnect.TabIndex = 1;
+            this.vpmDisconnect.Text = "vpnDisconnect";
+            this.vpmDisconnect.UseVisualStyleBackColor = true;
+            this.vpmDisconnect.Click += new System.EventHandler(this.vpmDisconnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 222);
+            this.Controls.Add(this.vpmDisconnect);
+            this.Controls.Add(this.vpnConnect);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button vpnConnect;
+        private System.Windows.Forms.Button vpmDisconnect;
     }
 }
 
--- VPN Connection/VPN Connection/Form1.cs
+++ VPN Connection/VPN Connection/Form1.cs
@@ -1,74 +1,100 @@
 ﻿using DotRas;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.NetworkInformation;
 using System.Windows.Forms;
-using static System.Net.WebRequestMethods;
 
-namespace VPN_Connection {
-    public partial class Form1 : Form {
-        public Form1() {
+namespace VPN_Connection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
             InitializeComponent();
-            /*foreach(RasDevice RasDevice in RasDevice.GetDevices()) {
-                Console.WriteLine(RasDevice.Name);
-                RasEntry entry = RasEntry.CreateVpnEntry("My VPN Client", "petrolcard.hu", RasVpnStrategy.PptpFirst, RasDevice); //Prepare server details
-                Console.WriteLine(entry.Device.Name);
-            }*/
+            Timer timer = new Timer();
+            timer.Interval = 2000;
+            timer.Tick += (sender, args) => getConnectionStatus(sender, args);
+            timer.Start();
+
+            //HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles
         }
 
 
-            RasDialer dialer = new RasDialer();
-            void connectPPTP(string ip, string username, string password){
+        RasDialer dialer = new RasDialer();
+        void connectPPTP(string ip, string username, string password)
+        {
+            Uri uri = new Uri(ip);
+            ip = Dns.GetHostAddresses(uri.Host)[0].ToString();
             FileStream fs = null;
             String path = @".\\vpn.pbk";
-            if(System.IO.File.Exists(path)) {
+            if (System.IO.File.Exists(path))
+            {
                 System.IO.File.Delete(path);
             }
-                fs = System.IO.File.Create(path);
-                fs.Close();
+            fs = System.IO.File.Create(path);
+            fs.Close();
 
-            
-                        //File.WriteAllText("vpn.pbk", ""); //Clean & create vpn.pbk
-                        RasPhoneBook book = new RasPhoneBook();
-                book.Open("vpn.pbk"); //Define book path
-            RasDevice device = RasDevice.GetDeviceByName("(PPTP)", RasDeviceType.Vpn); //PPTP interface in Windows
-            RasEntry entry = RasEntry.CreateVpnEntry("My VPN Client", ip, RasVpnStrategy.PptpFirst, device); //Prepare server details
-                book.Entries.Add(entry); //Write to vpn.pbk
 
-    //Writing server information is done, now we will connect to it.
-
-                
-                dialer = new RasDialer();
-                            dialer.PhoneBookPath = book.Path; //Read server list from vpn.pbk
-                dialer.Credentials = new NetworkCredential(username, password); //Define username and password
-                dialer.EntryName = "My VPN Client"; //Get server named My VPN Client
-                dialer.Dial(); //Connect
-            }
-
-void disconnectPPTP()
-{
-                RasConnection conn = RasConnection.GetActiveConnections().Where(o => o.EntryName == "My VPN Client").FirstOrDefault(); //Used LINQ, to get connection named My VPN Client
-                if(conn != null) //If connection is found
+            //File.WriteAllText("vpn.pbk", ""); //Clean & create vpn.pbk
+            RasPhoneBook book = new RasPhoneBook();
+            book.Open(path); //Define book path
+            bool first = true;
+            RasDevice device = null;
+            foreach (RasDevice _device in RasDevice.GetDevices())
+            {
+                Console.WriteLine(_device.Name+" "+_device.DeviceType);
+                if (_device.Name.Contains("PPTP") && _device.DeviceType.ToString().ToLower() == "vpn" && first)
                 {
-                    conn.HangUp(); //You know what this does
+
+                    device = _device;
+                    first = false;
                 }
             }
+            /*RasDevice device = RasDevice.GetDeviceByName("(PPTP)", RasDeviceType.Vpn); //PPTP interface in Windows*/
+            RasEntry entry = RasEntry.CreateVpnEntry("Petrolcard.hu", ip, RasVpnStrategy.PptpFirst, device); //Prepare server details
+            book.Entries.Add(entry); //Write to vpn.pbk
 
-            bool getConnectionStatus()
-{
-                RasConnection conn = RasConnection.GetActiveConnections().Where(o => o.EntryName == "My VPN Client").FirstOrDefault(); //Used LINQ, to get connection named My VPN Client
-                if(conn != null) //If connection is found
-                {
-                    return true;
-                }
-                return false;
+            //Writing server information is done, now we will connect to it.
+
+
+            dialer = new RasDialer();
+            dialer.PhoneBookPath = book.Path; //Read server list from vpn.pbk
+            dialer.Credentials = new NetworkCredential(username, password); //Define username and password
+            dialer.EntryName = "Petrolcard.hu"; //Get server named My VPN Client
+            dialer.Dial(); //Connect
+        }
+
+        void disconnectPPTP()
+        {
+            RasConnection conn = RasConnection.GetActiveConnections().Where(o => o.EntryName == "Petrolcard.hu").FirstOrDefault(); //Used LINQ, to get connection named My VPN Client
+            if (conn != null) //If connection is found
+            {
+                conn.HangUp(); //You know what this does
             }
+        }
+
+        void getConnectionStatus(object sender, EventArgs args)
+        {
+            RasConnection conn = RasConnection.GetActiveConnections().Where(o => o.EntryName == "Petrolcard.hu").FirstOrDefault(); //Used LINQ, to get connection named My VPN Client
+            if (conn != null) //If connection is found
+            {
+                Console.WriteLine("VPN aktív");
+                this.BackColor = System.Drawing.Color.Green;
+            }
+            else Console.WriteLine("VPN inaktív"); ;
+        }
+
+        private void vpnConnect_Click(object sender, EventArgs e)
+        {
+            connectPPTP("http://www.petrolcard.hu", "jundjkft", "jundjkft");
+        }
+
+        private void vpmDisconnect_Click(object sender, EventArgs e)
+        {
+            disconnectPPTP();
+        }
     }
 }
